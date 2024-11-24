74076de704af35e61732ce767a52ee8f1f49677e
Askar <ga.askar@zohomail.com>
2024-11-25 04:14:59 +0500

    Первоначальная версия проекта

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useHead } from '@unhead/vue';
+
+useHead({
+  script: [
+    {
+      src: 'https://telegram.org/js/telegram-web-app.js',
+      defer: true,
+    },
+  ],
+});
+</script>
+
+<template>
+  <div>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+      <h1>Hello Telegram</h1>
+    </section>
+  </template>
+  
+  <script setup lang="ts">
+    // Можно добавить код на TypeScript, если это необходимо
+  </script>
+  
+  <style scoped>
+    /* Добавьте стили для компонента */
+  </style>
+  
+  
